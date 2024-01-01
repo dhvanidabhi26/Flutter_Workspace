5f12f60dd5bdc784e14288aea04f778295e6690d
dhvanidabhi26 <154655443+dhvanidabhi26@users.noreply.github.com>
2024-01-01 13:35:58 +0530

    Add files via upload

---
+++ 30 dec/task/task C/for loop.dart
@@ -0,0 +1,10 @@
+// For Loop
+
+void main()
+{
+  int i = 100;
+  for (i = 100; i >= 81; i--)
+  {
+    print(i);
+  }
+}
---
+++ 30 dec/task/task C/while loop.dart
@@ -0,0 +1,11 @@
+void main()
+{
+
+  int i=100;
+
+  while(i>=81)
+    {
+      print(i);
+      i--;
+    }
+}
