a529c8a7fefb06cd912bd576fa3c9e95ecd1cc3a
dhvanidabhi26 <154655443+dhvanidabhi26@users.noreply.github.com>
2024-01-16 14:32:11 +0530

    Add files via upload

---
+++ module1/28 dec/positive nagative.dart
@@ -0,0 +1,22 @@
+// Positive Nagative
+
+import 'dart:io';
+
+void main()
+{
+  print ("Emter Any Number");
+  int num = int.parse(stdin.readLineSync().toString());
+
+  if(num>0)
+  {
+    print("Number is positive");
+  }
+  else if (num==0)
+  {
+    print ("Number is Neutral or zero");
+  }
+  else
+  {
+    print("Number is Negative");
+  }
+}
