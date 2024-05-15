f0406ff7d18843a2c26ddcab7cddc63ec7154e7f
dhvanidabhi26 <154655443+dhvanidabhi26@users.noreply.github.com>
2024-05-15 11:26:36 +0530

    Update Q-58.dart

--- module3/assignment/Q-58.dart
+++ module3/assignment/Q-58.dart
@@ -1,46 +1,130 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class SeekBarExample extends StatefulWidget {
+
+class SeekBar extends StatefulWidget
+{
   @override
-  _SeekBarExampleState createState() => _SeekBarExampleState();
+  SeekBarState createState() => SeekBarState();
+
 }
 
-class _SeekBarExampleState extends State<SeekBarExample> {
-  double _currentValue = 0;
+class SeekBarState extends State<SeekBar>
+{
+  double redValue = 0.0;
+  double greenValue = 0.0;
+  double blueValue = 0.0;
+
+
+  void updateBackgroundColor()
+  {
+
+    setState(() {
+
+
+    });
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Voice'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Voice: $_currentValue',
-              style: TextStyle(fontSize: 20.0),
-            ),
+  Widget build(BuildContext context)
+  {
+  final backgroundColor = Color.fromRGBO(
+    redValue.toInt(),
+    greenValue.toInt(),
+    blueValue.toInt(),
+    1.0,
+  );
 
-            SizedBox(height: 20.0),
-            Slider(
-              value: _currentValue,
-              min: 0,
-              max: 100,
-              divisions: 100,
-              onChanged: (double value)
-              {
-                setState(() {
-                  _currentValue = value;
-                });
-              },
-            ),
-          ],
+    return Scaffold(
+      resizeToAvoidBottomInset : false,
+      appBar: AppBar(
+        title: Text("SeekBar"),
+      ),
+      body: SingleChildScrollView(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+
+          children: [
+
+            SizedBox(height: 20),
+
+
+                  Text(
+                    'Red: ${redValue.toInt()}',
+                    style: TextStyle(color: Colors.red),
+                  ),
+
+                  Slider(value: redValue,
+                    onChanged: (newValue)
+                    {
+                      setState(() {
+                        redValue = newValue;
+                        updateBackgroundColor();
+                      });
+                    },
+                    min: 0.0, // Minimum value
+                    max: 100.0, // Maximum value
+                    divisions: 100, // Number of discrete divisions
+                    label: redValue.toString(),
+                  ),
+
+                  Text(
+                    'Green: ${greenValue.toInt()}',
+                    style: TextStyle(color: Colors.green),
+                  ),
+
+                  Slider(value: greenValue,
+                    onChanged: (newValue)
+                    {
+                      setState(() {
+                        greenValue = newValue;
+                        updateBackgroundColor();
+                      });
+
+                    },
+                    min: 0.0, // Minimum value
+                    max: 100.0, // Maximum value
+                    divisions: 100, // Number of discrete divisions
+                    label: greenValue.toString(),
+                  ),
+
+                  Text(
+                    'Blue: ${blueValue.toInt()}',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+
+                  Slider(value: blueValue,
+                    onChanged: (newValue)
+                    {
+                      setState(() {
+                        blueValue = newValue;
+                        updateBackgroundColor();
+                      });
+
+                    },
+                    min: 0.0, // Minimum value
+                    max: 100.0, // Maximum value
+                    divisions: 100, // Number of discrete divisions
+                    label: blueValue.toString(),
+                  ),
+
+              SizedBox(height: 20),
+              Container(
+                width: 500,
+                height: 500,
+                color: backgroundColor,
+
+              ),
+        ],
         ),
       ),
+
     );
   }
+
 }
+          
 
 
