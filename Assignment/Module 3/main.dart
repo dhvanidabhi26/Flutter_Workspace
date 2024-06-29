50b0168c5c51a9d0cdfb30a624018ad8efcabf2f
dhvanidabhi26 <154655443+dhvanidabhi26@users.noreply.github.com>
2024-06-29 10:38:40 +0530

    commit

---
+++ Assignment/Module 3/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'Q-49informationpage.dart';
+import 'Q-49mainpage.dart';
+import 'Q-50.dart';
+import 'Q-51.dart';
+import 'Q-52.dart';
+import 'Q-53.dart';
+import 'Q-54.dart';
+import 'Q-55.dart';
+import 'Q-56.dart';
+import 'Q-57.dart';
+import 'Q-58.dart';
+import 'Q-59loginpage.dart';
+import 'Q-60mainpage.dart';
+
+
+
+void main()
+{
+  runApp(MaterialApp(home: Restaurant(),
+    debugShowCheckedModeBanner: false,));
+}
---
+++ Assignment/Module 3/model.dart
@@ -0,0 +1,16 @@
+class Model
+{
+  late String images;
+  late String title;
+  late String subtitle;
+  late String price;
+
+  Model({
+
+    required this.images,
+    required this.title,
+    required this.subtitle,
+    required this.price
+  });
+
+}
